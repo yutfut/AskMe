31fc5fc0f38b993d3940bde09942b22967ffb5a8
yut_fut <seregey.golubev@mail.ru>
2021-12-24 10:56:01 +0300

    new manage

--- askme/settings.py
+++ askme/settings.py
@@ -1,8 +1,7 @@
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,12 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',]
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
